630238d2dfc7eccea0022702b1c882066e6364ff
patricio <patricioa@ecorporativa.com>
2014-08-26 10:19:17 -0500

    Version inicial proyecto maven

---
+++ excorejmplo/src/main/java/excor/Main.java
@@ -0,0 +1,19 @@
+package excor;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		VentaData data=new VentaData();
+		mostrarMensajes();
+		data.muestraData();
+
+	}
+	private static void mostrarMensajes(){
+		System.out.println("ESTE ES UN EJEMPLO DE PROYECTO MAVEN CON REPOSITORIO GIT");
+		System.out.println("ESPEREMOS FUNCIONE");
+	}
+
+}
---
+++ excorejmplo/src/main/java/excor/VentaData.java
@@ -0,0 +1,12 @@
+package excor;
+
+public class VentaData {
+
+	int data[]={0,4,42};
+	public void muestraData() {
+		System.out.println("TIPO: Implementacion data muestraData");
+		
+
+	}
+
+}
