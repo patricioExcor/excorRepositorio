c76f1c51aebc66e33583c0bf7f4d376685e0989e
CRIS <cristina88alexa@hotmail.com>
2014-08-26 12:10:23 -0500

    Prueba Cris

--- excorejmplo/src/main/java/excor/Main.java
+++ excorejmplo/src/main/java/excor/Main.java
@@ -16,6 +16,7 @@ public class Main {
 		System.out.println("ESPEREMOS FUNCIONE");
 		System.out.println("LISTO NUESTRO GIT REPOSITORIO LOCAL Y REMOTE");
 		System.out.println("HOLA CRISS");
+		System.out.println("HOLA PATOOOOO");
 	}
 
 }
