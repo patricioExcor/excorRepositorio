a63b4c0fbc43e656099d40940079ca85ea841045
patricio <patricioa@ecorporativa.com>
2014-08-26 11:41:48 -0500

    aumente una linea de mensaje en ventadata.java

--- excorejmplo/src/main/java/excor/VentaData.java
+++ excorejmplo/src/main/java/excor/VentaData.java
@@ -5,6 +5,7 @@ public class VentaData {
 	int data[]={0,4,42};
 	public void muestraData() {
 		System.out.println("TIPO: Implementacion data muestraData");
+		System.out.println("Otra prueba mas");
 		
 
 	}
